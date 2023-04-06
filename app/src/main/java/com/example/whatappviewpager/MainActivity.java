654978c7bae4a055ce943f13a987665d45a5a927
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-04-06 12:02:57 +0530

    Created new project viewPager

---
+++ app/src/main/java/com/example/whatappviewpager/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.whatappviewpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.example.whatappviewpager.WhatsApp.MyViewPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager2 viewPagerVg;
+    MyViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        setUpAdapter();
+        connectAdapter();
+    }
+
+    private void connectAdapter() {
+        viewPagerVg.setAdapter(adapter);
+    }
+
+    private void setUpAdapter() {
+        adapter = new MyViewPagerAdapter(this);
+    }
+
+    private void initViews() {
+        viewPagerVg = findViewById(R.id.viewpager_vg);
+    }
+}
